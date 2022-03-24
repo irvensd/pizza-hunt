dc4c284726c0096f9fb83ff3d7f63b8dfdafcb45
Irvens R Dupuy <92906651+irvensd@users.noreply.github.com>
2022-03-24 13:05:51 -0400

    Validation

--- models/Comment.js
+++ models/Comment.js
@@ -9,10 +9,13 @@ const ReplySchema = new Schema(
       default: () => new Types.ObjectId()
     },
     replyBody: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     writtenBy: {
-      type: String
+      type: String, 
+      required: true
     },
     createdAt: {
       type: Date,
@@ -32,7 +35,7 @@ const ReplySchema = new Schema(
 const CommentSchema = new Schema(
   {
     writtenBy: {
-      type: String
+      type: String,
     },
     commentBody: {
       type: String
